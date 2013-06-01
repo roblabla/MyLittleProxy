8365300359500a5e4b25efe6718d62a5289addc0
Robin Lambertz <robinpub@googlemail.com>
2013-06-01 15:13:55 +0100

    PureJS implementation of RSA through my repository

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 var mc = require('minecraft-protocol')
   , fs = require('fs');
 
+// Put this in an external YAML file.
 var options = {
-  username: "your minecraft username",
-  password: "your minecraft password",
-  host: "server to connect to.",
+  username: "Your username here",
+  password: "Your password here",
+  host: "Server to debug here",
   port: 25565, // port to connect to
   whitelistip: "" // This is to prohibit anyone from using your debug proxy. Leave blank to allow anyone to access
 }
 
+// Make this more configurable
 var writestream = fs.createWriteStream('dump.txt', {flags: 'w', encoding: 'utf8'});
 
+console.log("Creating server...");
+
+// Put this in an external YAML file.
 var server = mc.createServer({
   host: '0.0.0.0',
   port: 25565,
@@ -19,8 +24,10 @@ var server = mc.createServer({
   'online-mode': false,
 });
 
+console.log("Server listening on " + '0.0.0.0' + ":" + 25565);
 
 server.on('login', function(client) {
+  console.log("Logging in");
   if (options.whitelistip !== "" && client.socket.remoteAddress != options.whitelistip) {
     client.end("You aren't whitelisted.");
     return;
@@ -29,22 +36,22 @@ server.on('login', function(client) {
   var remoteclient = mc.createClient(options);
   remoteclient.once('login', function() {
     ready = true;
-    console.log("remote is ready");
+    console.log("All clear !");
   });
   remoteclient.on('packet', function(packet) {
+    packet.destination = "local";
+    writestream.write(JSON.stringify(packet) + "\n");
     if (!ready) {
       return;
     }
-    packet.destination = "local";
-    writestream.write(JSON.stringify(packet) + "\n");
     client.write(packet.id, packet);
   });
   client.on('packet', function(packet) {
+    packet.destination = "remote";
+    writestream.write(JSON.stringify(packet) + "\n");
     if (!ready) {
       return;
     }
-    packet.destination = "remote";
-    writestream.write(JSON.stringify(packet) + "\n");
     remoteclient.write(packet.id, packet);
   });
   remoteclient.on('error', function(err) {
@@ -61,7 +68,6 @@ server.on('login', function(client) {
   client.on('end', function(reason) {
     remoteclient.end('[Local] ' + reason);
   });
-  console.log("Ready");
 });
 
 server.on('error', function(error) {
